4b768ab694a7901cff97ffdaa617627873d92282
franciscocamellon <franciscocamellon@gmail.com>
2020-09-25 23:38:16 -0300

    5 finished

--- questao_5.py
+++ questao_5.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 /***************************************************************************
 *    Questao 05                                                            *
 *        Aluno           : Francisco Alves Camello Neto                    *
@@ -7,7 +7,7 @@
 *        Professor       : Carlos Pivotto                                  *
 *        Nome do arquivo : questao_05.py                                   *
 ***************************************************************************/
-"""
+'''
 import csv
 import matplotlib.pyplot as plotter
 
@@ -19,35 +19,54 @@ class Zero_Error(Exception):
 
 
 class World_PIB_Projection():
-    """
-    """
+    '''
+    '''
 
     def __init__(self):
-        """
+        '''
         Constructor
-        """
-        self.label = []
-        self.eixoXAno = []
-        self.eixoYPib = []
-        self.input = {'country': ['  Informe um país:  ', ''],
-                     'year': ['  Informe um ano entre 2013 e 2020: ', '']}
+        '''
+        self.report = []
+        self.x_axis = []
+        self.y_axis = []
+        self.input = {'country': ['  Informe um país: ', ''],
+                      'year': ['  Informe um ano entre 2013 e 2020: ', '']}
         self.file = []
         self.file_reader = {}
-        
+        self.data = {'País': [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]}
+
+    def open_file(self):
+        """
+        This function reads the CSV file and fills the self.data
+        variable with its contents.
+        """
+
+        with open('Assessment_PIBs.csv', newline='', encoding='utf-8') as csvfile:
+            file_reader = csv.DictReader(csvfile)
+
+            for column in file_reader:
+                self.data[column.get('País')] = [
+                    float(column.get('2013')), float(column.get('2014')),
+                    float(column.get('2015')), float(column.get('2016')),
+                    float(column.get('2017')), float(column.get('2018')),
+                    float(column.get('2019')), float(column.get('2020')),
+                ]
+
+            return self.data
 
     def error(self, value):
-        """
+        '''
         Raises a customized exception.
-        """
+        '''
         if isinstance(value, str):
             return value
         else:
             raise Zero_Error()
 
     def validate_values(self, value, string, title):
-        """
+        '''
         Validates the input value from wage given by the user.
-        """
+        '''
         while True:
             try:
                 value = str(input(title))
@@ -60,65 +79,56 @@ class World_PIB_Projection():
                 print('Digite o nome de um país!')
 
     def init_class(self):
+        """
+        This function receives the iputs from users and links other methods
+        from this class.
+        """
 
-        with open('Assessment_PIBs.csv', newline="", encoding="utf-8") as csvfile:
-            self.file = csv.DictReader(csvfile)
+        self.open_file()
 
-            for k, v in self.input.items():
-                self.validate_values(v[1], k, v[0])
-            
-            for coluna in self.file_reader:
-                if coluna.get("País").lower() == self.input.get('country')[1].lower():
-                    paisSelecionado = coluna.get("País")
-                    ano = self.input.get('year')[1]
-                    pibPais = coluna.get(ano)
-                    print("\n  PIB {} em {}: US${} trilhões.".format(
-                        paisSelecionado, ano, pibPais))
-                    break
+        for k, v in self.input.items():
+            self.validate_values(v[1], k, v[0])
 
-    
+        for k, v in self.data.items():
+            year = int(self.input.get('year')[1])
+            country = self.input.get('country')[1].lower()
+            if k == 'País' and year in v:
+                idx = v.index(year)
+                self.report.insert(1, year)
+            elif k.lower() == country:
+                self.report.insert(0, k)
+                self.report.insert(2, v[idx])
+            else:
+                break
+        print(
+            '\n  O PIB do(a) {} em {}: US$ {} trilhões.'.format(
+                self.report[0], self.report[1], self.report[2]))
 
-    def process_data(self):
-        with open('Assessment_PIBs.csv', newline="", encoding="utf-8") as csvfile:
-            self.file_reader = csv.DictReader(csvfile)
+        return self.report
 
-            for coluna in self.file_reader:
-                primeiroAno = float(coluna.get("2013").replace(",", "."))
-                ultimoAno = float(coluna.get("2020").replace(",", "."))
-                variacao = (ultimoAno * 100 / primeiroAno) - 100
-                paisAtual = coluna.get("País")
-                print("  {}: Variação de {:.2f}% entre 2013 e 2020.".format(paisAtual, variacao))
+    def growth_data(self):
 
-    def plot_data(self):
-        eixoXAno, eixoYPib = [], []
-        with open('Assessment_PIBs.csv', newline="", encoding="utf-8") as csvfile:
-            self.file_reader = csv.DictReader(csvfile)
+        for k, v in self.data.items():
+            if k != 'País':
+                growth = (v[7]*100 / v[0])-100
+                print(
+                    '  {}: obteve variação de {:.2f}% no PIB entre 2013 e 2020.'
+                    .format(k, growth))
 
-            for coluna in self.file_reader:
-                if coluna.get("País").lower() == self.input.get('country')[1].lower():
-                    print(coluna)
-
-                    for index in range(1, len(coluna.values())): # percorrendo a lista gerada por cada coluna a partir dos anos
-                        valorAtualPib = list(coluna.values())
-                        dataAtual = list(coluna.keys())
-                        eixoYPib.append(float(valorAtualPib[index]))
-                        eixoXAno.append(int(dataAtual[index]))
-                    break
-        
-        plotter.plot(eixoXAno, eixoYPib)
+        self.y_axis = (self.data.get(self.report[0])[0:])
+        self.x_axis = (self.data.get('País')[0:])
+        plotter.plot(self.x_axis, self.y_axis)
         plotter.show()
 
     def print_result(self):
-        """
-        """
+        '''
+        '''
         print('===' * 25, '{:^63}'.format('Questão 04'), '===' * 25, sep='\n')
-        
         self.init_class()
-        print('---' * 25, ' Estimativa de variação do PIB, entre 2013 e 2020', sep='\n')
-        self.process_data()
+        print('---' * 25, ' Estimativa de variação do PIB, entre 2013 e 2020\n', sep='\n')
+        self.growth_data()
         print('---' * 25,
-              '{:>63}'.format('Aluno: Francisco Camello'), sep="\n")
-        self.plot_data()
+              '{:>63}'.format('Aluno: Francisco Camello'), sep='\n')
 
 
 World_PIB_Projection().print_result()
--- teste.py
+++ teste.py
@@ -1,34 +1,129 @@
 # -*- coding: utf-8 -*-
-
+'''
+/***************************************************************************
+*    Questao 05                                                            *
+*        Aluno           : Francisco Alves Camello Neto                    *
+*        Disciplina      : AT - Lógica, Computação e Algoritmos            *
+*        Professor       : Carlos Pivotto                                  *
+*        Nome do arquivo : questao_05.py                                   *
+***************************************************************************/
+'''
 import csv
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plotter
 
-def evolucaoPibGraph(pais):
-    """
-        Função que plota um gráfico, usando matplotlib, para mostrar a evolução do
-        pib do país selecionado.
-        http://www.funag.gov.br/ipri/images/analise-pesquisa/tabelas/top15pib.pdf
-        do FMI. Solicita um arquivo .csv como parâmetro e o nome do país.
-    """
-    eixoXAno = []
-    eixoYPib = []
 
-    with open('Assessment_PIBs.csv', newline="", encoding="utf-8") as csvfile:
-        arquivoPibs = csv.DictReader(csvfile)
-        
+class Zero_Error(Exception):
 
-        for coluna in arquivoPibs:
-            print(coluna)
-            if coluna.get("País").lower() == pais.lower():
+    def __init__(self):
+        super().__init__()
 
-                for index in range(1, len(coluna.values())): # percorrendo a lista gerada por cada coluna a partir dos anos
-                    valorAtualPib = list(coluna.values())
-                    dataAtual = list(coluna.keys())
-                    eixoYPib.append(float(valorAtualPib[index]))
-                    eixoXAno.append(int(dataAtual[index]))
+
+class World_PIB_Projection():
+    '''
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.report = []
+        self.growth = []
+        self.x_axis = []
+        self.y_axis = []
+        self.input = {'country': ['  Informe um país:  ', ''],
+                      'year': ['  Informe um ano entre 2013 e 2020: ', '']}
+        self.file = []
+        self.file_reader = {}
+        self.data = {'País': [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]}
+
+    def error(self, value):
+        '''
+        Raises a customized exception.
+        '''
+        if isinstance(value, str):
+            return value
+        else:
+            raise Zero_Error()
+
+    def validate_values(self, value, string, title):
+        '''
+        Validates the input value from wage given by the user.
+        '''
+        while True:
+            try:
+                value = str(input(title))
+                self.error(value)
+                self.input[string][1] = value
                 break
+            except TypeError:
+                print('Digite um número!')
+            except Zero_Error:
+                print('Digite o nome de um país!')
 
-    plt.plot(eixoXAno, eixoYPib)
-    plt.show()
+    def open_file(self):
+        """
+        This function reads the CSV file and fills the self.data
+        variable with its contents.
+        """
 
-evolucaoPibGraph('Indonésia')
+        with open('Assessment_PIBs.csv', newline='', encoding='utf-8') as csvfile:
+            file_reader = csv.DictReader(csvfile)
+
+            for column in file_reader:
+                self.data[column.get('País')] = [
+                    float(column.get('2013')), float(column.get('2014')),
+                    float(column.get('2015')), float(column.get('2016')),
+                    float(column.get('2017')), float(column.get('2018')),
+                    float(column.get('2019')), float(column.get('2020')),
+                ]
+
+            return self.data
+
+    def init_class(self):
+        """
+        This function receives the iputs from users and links other methods
+        from this class.
+        """
+
+        self.open_file()
+
+        for k, v in self.input.items():
+            self.validate_values(v[1], k, v[0])
+
+        for k, v in self.data.items():
+            year = int(self.input.get('year')[1])
+            country = self.input.get('country')[1].lower()
+            if k == 'País' and year in v:
+                idx = v.index(year)
+                self.report.insert(1, year)
+            elif k.lower() == country:
+                self.report.insert(0, k)
+                self.report.insert(2, v[idx])
+            else:
+                break
+        print('\n  O PIB do(a) {} em {}: US${} trilhões.'.format(
+            self.report[0], self.report[1], self.report[2]))
+
+        return self.report
+
+    def growth_data(self):
+
+        self.init_class()
+
+        for k, v in self.data.items():
+            if k != 'País':
+                growth = (v[7]*100 / v[0])-100
+                print(
+                    '  {}: obteve variação de {:.2f}% no PIB entre 2013 e 2020.'
+                    .format(k, growth))
+
+        self.y_axis = (self.data.get(self.report[0])[0:])
+        self.x_axis = (self.data.get('País')[0:])
+        plotter.plot(self.x_axis, self.y_axis)
+        plotter.show()
+        print(self.x_axis, self.y_axis)
+
+    # def plot(self):
+
+
+World_PIB_Projection().growth_data()
